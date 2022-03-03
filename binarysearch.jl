6694e7274e54ec428b90f136e38810b5711d961c
Code Komali <code.komali@gmail.com>
2022-03-03 16:00:59 +0530

    initial commit

---
+++ binarysearch.jl
@@ -0,0 +1,50 @@
+using Test
+
+function binarySearch(arr::Vector, elem;
+    desc = true,
+    compareFn = desc ? (x, y) -> x > y : (x, y) -> x < y,
+    startindex = 1)
+    mid = ceil(Int, length(arr) / 2)
+    if mid == 0
+        -1
+    elseif arr[mid] == elem
+        startindex + mid - 1
+    elseif compareFn(arr[mid], elem)
+        binarySearch(arr[mid+1:end], elem,
+            desc = desc, compareFn = compareFn, startindex = startindex + mid)
+    else
+        binarySearch(arr[1:mid-1], elem,
+            desc = desc, compareFn = compareFn, startindex = startindex)
+    end
+end
+
+# Complex type
+struct Student
+    name::String
+    rollno::String
+    cgpa::Float64
+end
+
+
+sortedStudents = [
+    Student("pip", "s2020126", 9.9),
+    Student("jack", "s2020123", 8.9),
+    Student("mark", "s2020127", 7.9),
+    Student("tom", "s2020125", 6.9),
+    Student("jill", "s2020124", 4.9)
+]
+# comparison function
+function studentCompareFn(x::Student, y::Student)
+    x.cgpa > y.cgpa
+end
+
+@testset "Binary search" begin
+    @test binarySearch([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 7) == 4
+    @test binarySearch([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 10) == 1
+    @test binarySearch([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], 1) == 10
+    @test binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7, desc = false) == 7
+    @test binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10, desc = false) == 10
+    @test binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1, desc = false) == 1
+    @test binarySearch(sortedStudents, Student("mark", "s2020127", 7.9),
+        compareFn = studentCompareFn) == 3
+end
---
+++ mergesort.jl
@@ -0,0 +1,100 @@
+using Test
+
+# Iterative version of merge
+function merge(arr1::Vector, arr2::Vector, compareFn)
+    iarr1 = 1
+    iarr2 = 1
+    res = []
+    while iarr1 <= lastindex(arr1) && iarr2 <= lastindex(arr2)
+        if compareFn(arr1[iarr1], arr2[iarr2])
+            push!(res, arr1[iarr1])
+            iarr1 += 1
+        else
+            push!(res, arr2[iarr2])
+            iarr2 += 1
+        end
+    end
+    if iarr1 <= lastindex(arr1)
+        append!(res, arr1[iarr1:end])
+    else
+        append!(res, arr2[iarr2:end])
+    end
+end
+
+@testset "Merge-iterative tests" begin
+    @test merge([11, 6, 2], [9, 7, 5], (x, y) -> x > y) == [11,9,7,6,5,2]
+    @test merge([2,6,11], [5,7,9], (x, y) -> x < y) == [2,5,6,7,9,11]
+end
+
+# Recursive implementation of merge
+function merger(arr1::Vector, arr2::Vector, compareFn)
+    if length(arr1) == 0
+        arr2
+    elseif length(arr2) == 0
+        arr1
+    elseif compareFn(arr1[1], arr2[1])
+        [popfirst!(arr1); merger(arr1, arr2, compareFn)]
+    else
+        [popfirst!(arr2); merger(arr1, arr2, compareFn)]
+    end
+end
+
+@testset "Merge-recursive tests" begin
+    @test merger([11, 6, 2], [9, 7, 5], (x, y) -> x > y) == [11,9,7,6,5,2]
+    @test merger([2,6,11], [5,7,9], (x, y) -> x < y) == [2,5,6,7,9,11]
+end
+
+# Recursive implementation of mergesort
+function mergesortr(arr::Vector, desc = true, 
+    compareFn = desc ? (x,y)->x>y : (x,y) -> x<y)
+    pivot = length(arr) ÷ 2
+    if pivot == 0
+        arr
+    else
+        merger(mergesortr(arr[1:pivot], desc, compareFn),
+            mergesortr(arr[pivot+1:end], desc, compareFn),
+            compareFn)
+    end
+end
+
+# Complex type
+struct Student
+    name::String
+    rollno::String
+    cgpa::Float64
+end
+
+# instances of the complex type Student
+students = [
+    Student("jack","s2020123",8.9),
+    Student("jill","s2020124",4.9),
+    Student("tom","s2020125",6.9),
+    Student("pip","s2020126",9.9),
+    Student("mark","s2020127",7.9)
+]
+
+# expected result after sorting students (descending) by CGPA
+sortedStudents = [
+    Student("pip", "s2020126", 9.9),
+    Student("jack", "s2020123", 8.9),
+    Student("mark", "s2020127", 7.9),
+    Student("tom", "s2020125", 6.9),
+    Student("jill", "s2020124", 4.9)  
+]
+# comparison function
+function studentCompareFn(x::Student, y::Student)
+    x.cgpa > y.cgpa
+end
+
+# test for recursive mergesort
+@testset "Mergesort-recursive tests" begin
+    @test mergesortr([11, 9, 7, 10, 8, 6], true) == [11,10,9,8,7,6]
+    @test mergesortr([11, 9, 7, 10, 8, 6], false) == [6,7,8,9,10,11]
+    randArray = rand(Int8,10)
+    @test mergesortr(randArray, true) == sort(randArray,rev=true) # Note: our mergesort implementation by default prefers descending order
+    @test mergesortr(randArray, false) == sort(randArray)
+    @test mergesortr(students,nothing,studentCompareFn) == sortedStudents
+end
+
+
+
