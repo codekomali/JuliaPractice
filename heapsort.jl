49e424f4ad175206c342cb31401f0429df8f4902
Code Komali <code.komali@gmail.com>
2022-03-04 17:37:19 +0530

    Added tests and slight cleanup

--- heapsort.jl
+++ heapsort.jl
@@ -1,54 +1,57 @@
 using Test
 
-function insert!(heap, elm; 
-    min=true, 
-    compareFn= min ? (x,y)->x<y : (x,y)->x>y)
-    push!(heap,elm)
-    val = percolateup!(heap, min=min, compareFn=compareFn)
+function insert!(heap, elm;
+    min = true,
+    compareFn = min ? (x, y) -> x < y : (x, y) -> x > y)
+    push!(heap, elm)
+    val = percolateup!(heap, min = min, compareFn = compareFn)
     return val
 end
 
 function delete!(heap;
-    min=true, 
-    compareFn= min ? (x,y)->x<y : (x,y)->x>y)
+    min = true,
+    compareFn = min ? (x, y) -> x < y : (x, y) -> x > y)
     isempty(heap) && return
-    swap!(heap,1,lastindex(heap))
+    swap!(heap, 1, lastindex(heap))
     elem = pop!(heap)
-    percoloatedown!(heap, min=min, compareFn=compareFn)
+    percoloatedown!(heap, min = min, compareFn = compareFn)
     return elem
 end
 
-function percolateup!(heap; 
+function percolateup!(heap;
     curr = lastindex(heap),
-    min=true, 
-    compareFn= min ? (x,y)->x<y : (x,y)->x>y)
+    min = true,
+    compareFn = min ? (x, y) -> x < y : (x, y) -> x > y)
     parent = curr ÷ 2
-    if parent != 0 && compareFn(heap[curr],heap[parent])
+    if parent != 0 && compareFn(heap[curr], heap[parent])
         swap!(heap, curr, parent)
-        val = percolateup!(heap,curr=parent, min=min, compareFn=compareFn)
-        return val
+        percolateup!(heap, curr = parent, min = min, compareFn = compareFn)
     else
-        return heap
+        heap
     end
 end
 
-function percoloatedown!(heap; 
-    curr=1,
-    min=true, 
-    compareFn= min ? (x,y)->x<y : (x,y)->x>y)
-    lchild = curr * 2
-    if lchild > lastindex(heap)
-        return heap
-    end
+function findtargetchild(heap, lchild;
+    min = true,
+    compareFn = min ? (x, y) -> x < y : (x, y) -> x > y)
     rchild = lchild + 1
     if rchild > lastindex(heap)
-        targetchild = lchild
+        lchild
     else
-        targetchild = compareFn(heap[lchild],heap[rchild]) ? lchild : rchild
+        compareFn(heap[lchild], heap[rchild]) ? lchild : rchild
     end
-    if compareFn(heap[targetchild],heap[curr])
+end
+
+function percoloatedown!(heap;
+    curr = 1,
+    min = true,
+    compareFn = min ? (x, y) -> x < y : (x, y) -> x > y)
+    lchild = curr * 2
+    lchild > lastindex(heap) && return heap
+    targetchild = findtargetchild(heap, lchild, min = min, compareFn = compareFn)
+    if compareFn(heap[targetchild], heap[curr])
         swap!(heap, curr, targetchild)
-        percoloatedown!(heap,curr=targetchild,min=min,compareFn=compareFn)
+        percoloatedown!(heap, curr = targetchild, min = min, compareFn = compareFn)
     else
         return heap
     end
@@ -60,45 +63,52 @@ function swap!(heap, index1, index2)
     heap[index2] = temp
 end
 
-function add(a,b)
-    println(a,b)
-    a + b
-end
 
-function heapsort(arr::Vector; desc=true, min=!desc, compareFn= min ? (x,y)->x<y : (x,y)->x>y)
-    insertFn! = (_heap,_elm) -> insert!(_heap,_elm,min=min, compareFn = compareFn)
-    deleteFn! = (_heap) -> delete!(_heap, min=min, compareFn = compareFn)
-    heap = reduce(insertFn!,arr,init=[])
+function heapsort(arr::Vector; desc = true, min = !desc, compareFn = min ? (x, y) -> x < y : (x, y) -> x > y)
+    insertFn! = (_heap, _elm) -> insert!(_heap, _elm, min = min, compareFn = compareFn)
+    deleteFn! = (_heap) -> delete!(_heap, min = min, compareFn = compareFn)
+    heap = reduce(insertFn!, arr, init = [])
     res = []
     while !isempty(heap)
-        push!(res,deleteFn!(heap))
+        push!(res, deleteFn!(heap))
     end
     return res
 end
 
-heapsort([6,4,12,10,15,17,3])
-heapsort([6,4,12,10,15,17,3], desc=false)
+# Complex type
+struct Student
+    name::String
+    rollno::String
+    cgpa::Float64
+end
+# instances of the complex type Student
+students = [
+    Student("jack", "s2020123", 8.9),
+    Student("jill", "s2020124", 4.9),
+    Student("tom", "s2020125", 6.9),
+    Student("pip", "s2020126", 9.9),
+    Student("mark", "s2020127", 7.9)
+]
 
+# expected result after sorting students (descending) by CGPA
+sortedStudents = [
+    Student("pip", "s2020126", 9.9),
+    Student("jack", "s2020123", 8.9),
+    Student("mark", "s2020127", 7.9),
+    Student("tom", "s2020125", 6.9),
+    Student("jill", "s2020124", 4.9)
+]
+# comparison function for student
+function studentCompareFn(x::Student, y::Student)
+    x.cgpa > y.cgpa
+end
 
-# heap=[]
-# reduce(insert!,[6,4,12,10,15,17,3],init=heap)
-
-
-
-
-
-# insert!(heap, 6)
-# insert!(heap, 4)
-# insert!(heap, 12)
-# insert!(heap, 10)
-# insert!(heap, 15)
-# insert!(heap, 17)
-# insert!(heap, 3)
-
-# delete!(heap, min=false)
-
-# insert!(heap, 6, min=false)
-# insert!(heap, 4, min=false)
-# insert!(heap, 12, min=false)
-# insert!(heap, 10, min=false)
-# insert!(heap, 17, min=false)
+# tests for heapsort
+@testset "Heapsort tests" begin
+    @test heapsort([11, 9, 7, 10, 8, 6]) == [11, 10, 9, 8, 7, 6]
+    @test heapsort([11, 9, 7, 10, 8, 6], desc = false) == [6, 7, 8, 9, 10, 11]
+    randArray = rand(Int8, 10)
+    @test heapsort(randArray) == sort(randArray, rev = true) # Note: our heapsort implementation by default prefers descending order
+    @test heapsort(randArray, desc = false) == sort(randArray)
+    @test heapsort(students, compareFn = studentCompareFn) == sortedStudents
+end
